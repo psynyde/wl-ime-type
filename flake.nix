0d85a4229e8e60eb2a8b8ef812e08ba5186fcd4c
psynyde <pxynyde@proton.me>
2025-12-28 01:30:02 +0600

    Added README and LICENSE and packaged with nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "wl-ime-type-zig";
+  description = "wl-ime-type";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -16,18 +16,62 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        project = "wl-ime-type-zig";
+        project = "wl-ime-type";
+        zigDeps = pkgs.callPackage ./zig-deps.nix { };
       in
       {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = project;
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              zig.hook
+              scdoc
+              pkg-config
+              wayland-scanner
+            ];
+
+            buildInputs = with pkgs; [
+              wayland
+              wayland-protocols
+            ];
+
+            postPatch = ''
+              ln -s ${zigDeps} $ZIG_GLOBAL_CACHE_DIR/p
+            '';
+
+            zigBuildFlags = [ "-Doptimize=ReleaseFast" ];
+
+            postBuild = ''
+              scdoc < wl-ime-type.1.scd > wl-ime-type.1
+            '';
+
+            postInstall = ''
+              install -Dm644 wl-ime-type.1 -t $out/share/man/man1
+            '';
+
+            meta = with pkgs.lib; {
+              description = "IME typing tool for Wayland in zig";
+              homepage = "https://github.com/psynyde/${project}";
+              license = licenses.bsd2;
+              maintainers = with maintainers; [ psynyde ];
+              platforms = platforms.linux;
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = project;
           LSP_SERVER = "zls";
           packages = with pkgs; [
             zig
             zls
+            scdoc
+            zon2nix
 
             pkg-config
-
             wayland
             wayland-protocols
             wayland-scanner
@@ -36,6 +80,7 @@
             echo -e '(¬_¬") Entered ${project} :D'
           '';
         };
+
         formatter = treefmt-nix.lib.mkWrapper pkgs {
           projectRootFile = "flake.nix";
           programs = {
